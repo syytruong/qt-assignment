b9ed01402e176a283c5e9cec74c598f76a7c9d82
Sy Truong <sy.truong@detectorinspector.com.au>
2023-12-11 15:44:54 +0700

    FEAT: Implement BE routes

--- backend/src/routes/certificateRoutes.js
+++ backend/src/routes/certificateRoutes.js
@@ -1,1 +1,15 @@
-// TODO: Create Express routes that link to controller methods.
+const express = require('express');
+const router = express.Router();
+
+const certificateController = require('../controllers/certificateController');
+
+// POST route for creating a new certificate
+router.post('/', certificateController.createCertificate);
+
+// GET route for listing all certificates
+router.get('/', certificateController.listCertificates);
+
+// DELETE route for deleting a certificate by ID
+router.delete('/:id', certificateController.deleteCertificate);
+
+module.exports = router;

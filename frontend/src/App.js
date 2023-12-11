075705020665819db471a9cca61b2bfd4251d06e
Sy Truong <sy.truong@detectorinspector.com.au>
2023-12-11 17:21:29 +0700

    CHORE: change APp to useCallback in order to reload the list after creation succeed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
 import CertificateForm from './components/CertificateForm';
 import CertificateList from './components/CertificateList';
 
 function App() {
+    const [certificates, setCertificates] = useState([]);
+
+    const fetchCertificates = useCallback(async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/certificates');
+            setCertificates(response.data);
+        } catch (error) {
+            console.error('Error fetching certificates:', error);
+        }
+    }, []);
+
+    // Fetch certificates initially and after every creation
+    useEffect(() => {
+        fetchCertificates();
+    }, [fetchCertificates]);
+
     return (
         <div>
             <h1>Certificate Management System</h1>
-            <CertificateForm />
-            <CertificateList />
+            <CertificateForm onCertificateCreate={fetchCertificates} />
+            <CertificateList certificates={certificates} />
         </div>
     );
 }
---
+++ frontend/src/components/CertificateForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function CertificateForm({ onCertificateCreate }) {
+    const [formData, setFormData] = useState({ name: '', issuer: '', dateIssued: '' });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:3000/certificates', formData);
+            setFormData({ name: '', issuer: '', dateIssued: '' }); // Clear the form
+            onCertificateCreate(); // Update the list in parent component
+        } catch (error) {
+            console.error('Error creating certificate:', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                name="name" 
+                placeholder="Name" 
+                value={formData.name} 
+                onChange={handleChange} // Attach handleChange here
+            />
+            <input 
+                type="text" 
+                name="issuer" 
+                placeholder="Issuer" 
+                value={formData.issuer} 
+                onChange={handleChange} // And here
+            />
+            <input 
+                type="date" 
+                name="dateIssued" 
+                value={formData.dateIssued} 
+                onChange={handleChange} // And also here
+            />
+            <button type="submit">Create Certificate</button>
+        </form>
+    );
+}
+
+export default CertificateForm;
